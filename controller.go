618742ab4feafdb67339d0dbd430cafa63fb379f
Peter Elmers <peter.elmers@rice.edu>
2013-12-10 12:00:20 -0600

    controller interface added

---
+++ controller.go
@@ -0,0 +1,17 @@
+package gothello
+
+// Types that implement this interface are supported by the game.
+type Controller interface {
+    // When it's this controller's turn, the game calls its GetMove method.
+    // GetMove is given a pointer to the Board, and it must return a move.
+    // Return Bitboard(0) to indicate no move.
+    // Board does not enforce move legality, so please
+    // do so within the controller.
+    GetMove(*Board) Bitboard
+}
+
+// NullControl does not make any moves. Used for testing.
+type NullControl struct{}
+
+func NewNullControl() NullControl               { return NullControl{} }
+func (n NullControl) GetMove(b *Board) Bitboard { return Bitboard(0) }
