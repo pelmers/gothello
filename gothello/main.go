f5be21ffcb6f767e64796f25e770077c6ea18c6e
Peter Elmers <peter.elmers@rice.edu>
2013-12-11 11:26:33 -0600

    random v random running

--- gothello/main.go
+++ gothello/main.go
@@ -1,9 +1,15 @@
 package main
 
-import g "github.com/pelmers/gothello"
+import (
+    g "github.com/pelmers/gothello"
+    "fmt"
+)
 
 func main() {
-    game := g.InitBoard(g.NewHumanController(), g.NewHumanController())
+    //game := g.InitBoard(g.NewHumanController(), g.NewHumanController())
+    game := g.InitBoard(g.NewRandomAI(true), g.NewRandomAI(true))
     for game.PlayTurn() {
     }
+    fmt.Printf("%s\nWhite: %d\nBlack: %d\n",game,
+    game.GetScore(g.BLACK), game.GetScore(g.WHITE))
 }
