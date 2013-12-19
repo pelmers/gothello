fecd6698910ddc6ddaed3fa5d7ca6b3acc07ee32
Peter Elmers <peter.elmers@rice.edu>
2013-12-19 07:05:51 -0600

    command line option parsing

--- gothello/main.go
+++ gothello/main.go
@@ -2,13 +2,33 @@ package main
 
 import (
     "fmt"
+    "flag"
     g "github.com/pelmers/gothello"
 )
 
 func main() {
-    //game := g.InitGame(g.NewHumanController(), g.NewHumanController())
-    //game := g.InitGame(g.NewShallowAI(true), g.NewHumanController())
-    game := g.InitGame(g.NewShallowAI(true), g.NewRandomAI(true))
+    black := flag.String("black","human","Black controller: human, shallow, search, random")
+    white := flag.String("white","human","White controller: human, shallow, search, random")
+    show := flag.Bool("s",true,"show AI moves")
+    flag.Parse()
+    var bc, wc g.Controller
+    switch *black {
+    case "shallow":
+        bc = g.NewShallowAI(*show)
+    case "random":
+        bc = g.NewRandomAI(*show)
+    default:
+        bc = g.NewHumanController()
+    }
+    switch *white {
+    case "shallow":
+        wc = g.NewShallowAI(*show)
+    case "random":
+        wc = g.NewRandomAI(*show)
+    default:
+        wc = g.NewHumanController()
+    }
+    game := g.InitGame(bc, wc)
     for game.PlayTurn() {
     }
     fmt.Printf("%s%s", game, "\nGame over.\n")
